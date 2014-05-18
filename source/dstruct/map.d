811c48e9245d405ec428e4c77b2f000a20a6f97b
w0rp <devw0rp@gmail.com>
2014-05-18 13:47:01 +0100

    Move things around a little and add comments for documentation purposes.

--- source/dstruct/map.d
+++ source/dstruct/map.d
@@ -50,6 +50,14 @@ unittest {
 
 private enum thresholdMultiplier = 0.75;
 
+/**
+ * This struct implements a hashmap type, much like the standard associative
+ * array type.
+ *
+ * This map should be almost totally usable in @safe pure nothrow functions.
+ *
+ * An empty map will be a valid object, and will not result in any allocations.
+ */
 struct HashMap(K, V) {
     private Entry!(K, V)*[] bucket;
     private size_t _length;
@@ -460,39 +468,6 @@ unittest {
     assert(map[2] == 4);
 }
 
-/**
- * Get or create a value from/in an associative array.
- *
- * Given an associative array to modify, a key, and a
- * lazy evaluated default value, attempt to retrieve a value from
- * an associative array. If a value for the given key is not set,
- * set the provided default value in the associative array and
- * return that.
- *
- * The value will be returned by reference.
- *
- * Params:
- *     map = The associative array to modify.
- *     key = The key in the associative array.
- *     def = A lazy default value.
- *
- * Returns:
- *     A reference to the value in the associative array.
- */
-@safe pure nothrow
-ref V1 setDefault(K, V1, V2)(ref V1[K] map, K key, lazy V2 def)
-if (is(V2 : V1)) {
-    V1* valPtr = key in map;
-
-    if (valPtr != null) {
-        return *valPtr;
-    }
-
-    map[key] = def();
-
-    return map[key];
-}
-
 unittest {
     int[string] map = ["foo": 3];
 
@@ -845,6 +820,39 @@ unittest {
     auto keyRange = func().keys();
     auto valueRange = func().values();
     auto itemRange = func().items();
+}
+
+/**
+ * Get or create a value from/in an associative array.
+ *
+ * Given an associative array to modify, a key, and a
+ * lazy evaluated default value, attempt to retrieve a value from
+ * an associative array. If a value for the given key is not set,
+ * set the provided default value in the associative array and
+ * return that.
+ *
+ * The value will be returned by reference.
+ *
+ * Params:
+ *     map = The associative array to modify.
+ *     key = The key in the associative array.
+ *     def = A lazy default value.
+ *
+ * Returns:
+ *     A reference to the value in the associative array.
+ */
+@safe pure nothrow
+ref V1 setDefault(K, V1, V2)(ref V1[K] map, K key, lazy V2 def)
+if (is(V2 : V1)) {
+    V1* valPtr = key in map;
+
+    if (valPtr != null) {
+        return *valPtr;
+    }
+
+    map[key] = def();
+
+    return map[key];
 }
 
 /**
