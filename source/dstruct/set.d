0c0c20c8584687424f946736cabff7e941f8cecb
w0rp <devw0rp@gmail.com>
2014-05-26 15:23:04 +0100

    Use the dstruct map for implementing HashSet instead, enabling more nothrow and @nogc code. Remove opApply which makes meeting these constraints hard.

--- source/dstruct/set.d
+++ source/dstruct/set.d
@@ -4,6 +4,9 @@
  */
 module dstruct.set;
 
+import dstruct.support;
+import dstruct.map;
+
 /**
  * A garbage collected implementation of a hash set.
  *
@@ -11,7 +14,7 @@ module dstruct.set;
  */
 struct HashSet(T) {
 private:
-    void[0][T] _map;
+    HashMap!(T, void[0]) _map;
 public:
     /**
      * Add an element to this set if needed.
@@ -39,22 +42,21 @@ public:
      *
      * Returns: true if a value was removed.
      */
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     bool remove(ref T value) {
         return _map.remove(value);
     }
 
     /// ditto
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     bool remove(T value) {
         return remove(value);
     }
 
-    // BUG: This is not @safe because AA .length isn't at least @trusted.
     /**
      * Returns: The number of elements in this set.
      */
-    @trusted pure nothrow
+    @nogc @safe pure nothrow
     @property size_t length() const {
         return _map.length;
     }
@@ -62,7 +64,7 @@ public:
     /**
      * Returns: True if this set is empty.
      */
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     @property bool empty() const {
         return length == 0;
     }
@@ -78,26 +80,25 @@ public:
      *
      * Returns: true if the value is in the set.
      */
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     bool opBinaryRight(string op, T)(ref T value) const if(op == "in") {
         return cast(bool)(value in _map);
     }
 
     /// ditto
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     bool opBinaryRight(string op, T)(T value) const if(op == "in") {
         return opBinaryRight!("in", T)(value);
     }
 
-    // BUG: This is not nothrow because AA foreach isn't.
     /**
      * Returns: A mutable copy of this set.
      */
-    @safe pure
+    @safe pure nothrow
     HashSet!T dup() const {
         HashSet!T newSet;
 
-        foreach(value, _; _map) {
+        foreach(value; _map.keys()) {
             newSet.add(value);
         }
 
@@ -107,7 +108,7 @@ public:
     /**
      * Returns: A immutable copy of this set.
      */
-    @safe pure
+    @safe pure nothrow
     immutable(HashSet!T) idup() const {
         return dup;
     }
@@ -119,32 +120,15 @@ public:
      *
      * Returns: An immutable reference to this immutable set.
      */
-    @safe pure nothrow
+    @nogc @safe pure nothrow
     ref immutable(HashSet!T) idup() immutable {
         return this;
     }
 
     /**
-     * Provide foreach for sets.
-     */
-    int opApply(int delegate(ref T) dg) {
-        int result = 0;
-
-        foreach(value, _; _map) {
-            result = dg(value);
-
-            if (result) {
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    /**
      * Returns: True if two sets contain all equal values.
      */
-    @trusted pure
+    @nogc @safe pure nothrow
     bool opEquals(U)(const(HashSet!U) otherSet) const
     if (is(U : T) || is(T : U)) {
         static if (is(U : T)) {
@@ -152,7 +136,7 @@ public:
                 return false;
             }
 
-            foreach(value, _; otherSet._map) {
+            foreach(value; otherSet._map.keys()) {
                 if (value !in this) {
                     return false;
                 }
@@ -192,22 +176,6 @@ unittest {
 
     assert(set.remove(4));
     assert(!set.remove(3));
-}
-
-// Test foreach
-unittest {
-    HashSet!int set;
-
-    set.add(3);
-
-    int[] list;
-
-    foreach(value; set) {
-        list ~= value;
-    }
-
-    assert(list.length == 1);
-    assert(list[0] == 3);
 }
 
 // Test set length
@@ -280,4 +248,18 @@ unittest {
     rightSet.add(3);
 
     assert(leftSet == rightSet);
+}
+
+/**
+ * Produce a range through all the entries of a set.
+ *
+ * Params:
+ *     set = A set.
+ *
+ * Returns:
+ *     A ForwardRange over all the entries in the set.
+ */
+@nogc @safe pure nothrow
+auto entries(U)(auto ref inout(HashSet!U) set) {
+    return set._map.keys();
 }
